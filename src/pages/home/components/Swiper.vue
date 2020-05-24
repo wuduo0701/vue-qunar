adf21f5eead069e229516da9b5ec45903bf3e130
1970186643 <1970186643@qq.com>
2020-05-24 13:05:47 +0800

    首页轮播图完成

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,10 +1,8 @@
 <template>
-    <swiper ref="mySwiper" :options="swiperOptions">
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide>
-    <swiper-slide>Slide 5</swiper-slide>
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide v-for="item in swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl">
+    </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
 </template>
@@ -13,10 +11,31 @@ export default {
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOptions: {}
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/vc/6d/9f/35/b8ad5468f73fd60ec0ced086f6.jpg_92.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
+        }
+      ]
     }
   }
 }
 </script>
 <style lang="stylus" scoped>
+.swiper-img
+  width 100%
 </style>
