57ec6592c0bd496b08486a08dcf99c9c28e5098a
1970186643 <1970186643@qq.com>
2020-05-24 14:49:54 +0800

    modify

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 像素比：
 在html中(reset.css)
 1 rem = 50 px
+
+- 上传到分支上后切换为主分支
+1. git  checkout master
+同步代码
+2. git pull origin master
+合并分支
+3. git  merge dev
+查看状态
+4. git status
+push到远端
+5. git push origin master
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,16 +2,19 @@
   <div>
     <HomeHeader></HomeHeader>
     <HomeSwiper></HomeSwiper>
+    <HomeIcons></HomeIcons>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
