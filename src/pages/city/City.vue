b6e0a256e7c37ca940458f6572009010883d8392
1970186643 <1970186643@qq.com>
2020-05-26 12:39:17 +0800

    city增加搜索组件

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,12 +1,17 @@
 <template>
-  <CityHeader></CityHeader>
+  <div>
+    <CityHeader></CityHeader>
+    <CitySearch></CitySearch>
+  </div>
 </template>
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input type="text" class="input" placeholder="输入城市名">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~com-styles/varibles.styl'
+  .search
+    height 0.72rem
+    padding 0 .1rem
+    background $bgColor
+    .input
+      box-sizing border-box
+      width 100%
+      height .62rem
+      padding 0 .1rem
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+      color #666
+</style>
