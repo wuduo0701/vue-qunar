0dacc48823010b3e54e275d991049166af1ebbb1
1970186643 <1970186643@qq.com>
2021-04-23 15:02:28 +0800

    feat: 我的页面完成

--- src/pages/myInfo/myInfo.vue
+++ src/pages/myInfo/myInfo.vue
@@ -1,5 +1,73 @@
 <template>
-  <div>wode</div>
+  <div class="MyInfo">
+    <div class="header">
+      <div class="iconfont back-icon">&#xe624;</div>
+      <img src="https://s.qunarzz.com/usercenter_mobile/images/my/mybgnew-20161111.jpg" class="backImg" alt="背景">
+      <div class="info-content">
+        <div class="avator-container">
+          <img src="https://source.qunarzz.com/usercenter/touch/avatar.png" class="avator" alt="头像">
+          <span class="unLogin">登录/注册</span>
+        </div>
+      </div>
+    </div>
+    <div class="list">
+      <div class="list_name">
+        <span class="common wallet">
+          <i class="iconfont">&#xe773;</i>
+        </span>
+        我的钱包
+      </div>
+      <div class="list_money">
+        <div class="balance flex_cell">
+          <label class="number">2.00</label>
+          <label class="name">账户余额</label>
+        </div>
+        <div class="coupon flex_cell">
+          <label class="number">0</label>
+          <label class="name">优惠劵</label>
+        </div>
+        <div class="card flex_cell">
+          <label class="number">
+            <em>0</em>
+            <em class="my-unit">张</em>
+          </label>
+          <label class="name">银行卡</label>
+        </div>
+      </div>
+    </div>
+    <div class="list">
+      <div class="list_name">
+        <span class="common order">
+          <i class="iconfont">&#xe659;</i>
+        </span>
+        <span class="name">我的订单</span>
+        <i class="iconfont link">&#xe68c;</i>
+      </div>
+      <div class="list_name">
+        <span class="common comment">
+          <i class="iconfont">&#xe601;</i>
+        </span>
+        <span class="name">我的评论</span>
+        <i class="iconfont link">&#xe68c;</i>
+      </div>
+    </div>
+    <div class="list">
+      <div class="list_name">
+        <span class="common about">
+          <i class="iconfont">&#xe724;</i>
+        </span>
+        <span class="name">意见反馈</span>
+        <i class="iconfont link">&#xe68c;</i>
+      </div>
+      <div class="list_name">
+        <span class="common about">
+          <i class="iconfont">&#xe60e;</i>
+        </span>
+        <span class="name">关于</span>
+        <i class="iconfont link">&#xe68c;</i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +76,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" src="./myInfo.scss" scoped></style>
